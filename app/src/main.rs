f85d647d03a1fcce8f817c44395472b0b82c65d2
FaboBorgesLima <fabo.borges.lima@gmail.com>
2024-05-27 21:23:47 -0300

    adding crud methods

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,6 +1,7 @@
 mod store;
 
 use mysql::{OptsBuilder, Pool};
+use rust_decimal::Decimal;
 use store::store_product::StoreProduct;
 
 fn main() {
@@ -8,24 +9,20 @@ fn main() {
     let db_user_password =
         std::fs::read_to_string(std::env::var("MARIADB_PASSWORD_FILE").unwrap()).unwrap();
     let db_name = std::env::var("MARIADB_DATABASE").unwrap();
-    let db_socket_address = std::env::var("MARIADB_SOCKET_ADDRESS").unwrap();
+    let db_address = std::env::var("MARIADB_ADDRESS").unwrap();
+    let db_port: u16 = std::env::var("MARIADB_PORT").unwrap().parse().unwrap();
 
     let builder = OptsBuilder::new();
 
     let pool = Pool::new(
         builder
             .db_name(Some(db_name))
-            .ip_or_hostname(Some(db_socket_address))
+            .ip_or_hostname(Some(db_address))
             .pass(Some(db_user_password))
-            .user(Some(db_user_name)),
+            .user(Some(db_user_name))
+            .tcp_port(db_port),
     )
     .unwrap();
 
-    let conn = pool.get_conn().unwrap();
-
-    let res = StoreProduct::all(conn).unwrap();
-
-    let first_item = res.get(0).unwrap();
-
-    println!("{}", first_item.to_json());
+    let mut conn = pool.get_conn().unwrap();
 }
--- app/src/store/store_product.rs
+++ app/src/store/store_product.rs
@@ -1,4 +1,4 @@
-use mysql::{prelude::Queryable, Error, PooledConn};
+use mysql::{params, prelude::Queryable, Conn, Error, PooledConn};
 use rust_decimal::Decimal;
 
 #[derive(PartialEq, Eq)]
@@ -20,9 +20,17 @@ impl StoreProduct {
 
         );
     }
-    pub fn all(mut conn: PooledConn) -> Result<Vec<StoreProduct>, Error> {
+    pub fn new(price: Decimal, quantity: i32, product_name: String) -> StoreProduct {
+        StoreProduct {
+            price,
+            quantity,
+            product_id: 0,
+            product_name,
+        }
+    }
+    pub fn all(conn: &mut PooledConn) -> Result<Vec<StoreProduct>, Error> {
         conn.query_map(
-            "SELECT product_id,price , quantity, product_name FROM product",
+            "SELECT product_id, price, quantity, product_name FROM product",
             |(product_id, price, quantity, product_name)| StoreProduct {
                 product_id,
                 price,
@@ -30,5 +38,24 @@ impl StoreProduct {
                 quantity,
             },
         )
+    }
+    pub fn delete(conn: &mut PooledConn, store_prod: &StoreProduct) {
+        let _ = conn.exec_drop(
+            "DELETE FROM product WHERE product_id=:product_id",
+            params!("product_id"=> store_prod.product_id),
+        );
+    }
+    pub fn insert(&self, conn: &mut PooledConn) {
+        let _ = conn.exec_drop(
+            "INSERT INTO product (price,quantity,product_name) VALUES (:price, :quantity, :product_name)",
+            params!("price"=>self.price,"quantity"=>self.quantity,"product_name"=>&self.product_name),
+        );
+    }
+
+    pub fn update(&self, conn: &mut PooledConn) {
+        let _ = conn.exec_drop(
+            "UPDATE product SET price = :price,quantity = :quantity ,product_name = :product_name WHERE product_id = :product_id",
+            params!(":product_id"=>self.product_id,"price"=>self.price,"quantity"=>self.quantity,"product_name"=>&self.product_name),
+        );
     }
 }
